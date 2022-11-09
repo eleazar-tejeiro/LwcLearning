1636ad24fef9eaaafc0ec67b5eaa81b80b0b3183
Uriel Tejeiro <uriel.tejeiro@oktana.com>
2022-11-08 20:58:05 -0400

    finished querySelector demo

--- force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
+++ force-app/main/default/lwc/helloQuerySelector/helloQuerySelector.js
@@ -3,6 +3,8 @@ import {
 } from 'lwc';
 
 export default class HelloQuerySelector extends LightningElement {
+  isChildElementVisible = false;
+
   userNames = [
     "Jhon",
     "Smith",
@@ -17,6 +19,18 @@ export default class HelloQuerySelector extends LightningElement {
     console.log(name.innerText);
     Array.from(userElements).forEach((userElement) => {
       console.log(userElement.innerText);
+      userElement.setAttribute('title', userElement.innerText);
+      userElement.setAttribute('style', 'color: red');
     });
+
+    // LWC: Manual DOM Manipulation
+    const childElement = this.template.querySelector('.child');
+    if (this.isChildElementVisible) {
+      childElement.innerHTML = '<h1>Child Element</h1>';
+      this.isChildElementVisible = false;
+    } else {
+      childElement.innerHTML = '';
+      this.isChildElementVisible = true;
+    }
   }
 }
